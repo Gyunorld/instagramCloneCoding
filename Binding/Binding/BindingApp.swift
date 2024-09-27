66dbae5b26c014721433dded9c48b650b374cb0b
kimmingyu <mingue640@gmail.com>
2024-09-27 14:45:41 +0900

    사진업로드화면

---
+++ Binding/Binding/BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingApp.swift
+//  Binding
+//
+//  Created by 김민규 on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
